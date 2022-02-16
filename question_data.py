b7dd3240b25ab9c937bdbc45abb5fa52e021d8f8
Sam Adebisi <98032680+SamAdebisi@users.noreply.github.com>
2022-02-16 23:53:01 +0100

    Add files via upload

---
+++ question_data.py
@@ -0,0 +1,143 @@
+question_data = [
+    {
+        "category": "Science & Nature",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Doppler effect applies to light.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The film \"2001: A Space Odyssey\" was released on December 31st, 2000.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "\"Undertale\" is an RPG created by Toby Fox and released in 2015.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Assyrian king Sennacherib's "
+                    "destruction of Babylon in 689 BCE was viewed as a triumph by other Assyrian citizens.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "There are 86400 seconds in a day.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "The binary number \"101001101\" is equivalent to the Decimal number \"334\"",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Metal Gear Solid V: The Phantom Pain runs on the Fox Engine.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "category": "Science: Gadgets",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The communication protocol NFC stands for Near-Field Control.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "If you could fold a piece of paper in half 50 times, its'; "
+                    "thickness will be 3/4th the distance from the Earth to the Sun.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Vehicles",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The full English name of the car manufacturer BMW is Bavarian Motor Works",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Toronto is the capital city of the North American country of Canada.",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Oxford University is older than the Aztec Empire. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Brandon Routh plays the titular character in the movie \"John Wick\".",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Board Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "\"PAYDAY: The Heist\" is a sequel to the board game \"Payday\".",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In World War ll, Great Britain used inflatable tanks on the ports of Great Britain to divert "
+                    "Hitler away from Normandy D-day landing.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In RuneScape, one must complete the \"Dragon Slayer\" "
+                    "quest before equipping Rune Platelegs.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]},
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The character that would eventually become Dr. Eggman was considered for the role of Sega's "
+                    "new flagship mascot before Sonic was.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "General Knowledge",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The scientific name for the Southern Lights is Aurora Australis?",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Entertainment: Cartoon & Animations",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Nutcracker Suite was one of the musical pieces featured in Disney's "
+                    "1940's film Fantasia.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "In the game \"Until Dawn\" Emily is the only playable character who can be killed by "
+                    "another playable character directly.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
